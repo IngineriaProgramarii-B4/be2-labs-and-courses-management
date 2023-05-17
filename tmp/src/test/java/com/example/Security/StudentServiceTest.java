770bdb57a913caee4d4ad7f3241664c86a883856
ceciliaM0 <100302771+ceciliaM0@users.noreply.github.com>
2023-05-17 12:28:01 +0300

    Add files via upload

---
+++ tmp/src/test/java/com/example/Security/StudentServiceTest.java
@@ -0,0 +1,218 @@
+package com.example.demo;
+
+import com.example.demo.objects.Student;
+import com.example.demo.repositories.StudentRepository;
+import com.example.demo.services.StudentService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentServiceTest {
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentService studentService;
+
+    @BeforeEach
+    public void setUp() {
+        studentService = new StudentService(studentRepository);
+    }
+
+    @Test
+    public void testAddStudent() {
+        String registrationNumber = "A112";
+        String firstName = "John";
+        String lastName = "Doe";
+        int year = 2;
+        String grupa = "A2";
+        String mail = "johndoe@example.com";
+        String password = "password";
+
+        studentService.addStudent(registrationNumber, firstName, lastName, year, grupa, mail, password);
+        verify(studentRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void testGetStudentByRegistrationNumber() {
+        // Given
+        String registrationNumber = "A112";
+        String firstName = "John";
+        String lastName = "Doe";
+        int year = 2;
+        String grupa = "A2";
+        String mail = "johndoe@example.com";
+        String password = "password";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date createdAt= new Date(formatter.format(date));
+        Date updatedAt= new Date(formatter.format(date));
+
+        Student expectedStudent = new Student(registrationNumber, firstName, lastName, year, grupa, mail, password, createdAt, updatedAt);
+
+        when(studentRepository.findByRegistrationNumber(registrationNumber)).thenReturn(expectedStudent);
+
+        // When
+        Student retrievedStudent = studentService.getStudentByRegistrationNumber(registrationNumber);
+
+        // Then
+        Assertions.assertNotNull(retrievedStudent);
+        assertEquals(expectedStudent.getFirstName(), retrievedStudent.getFirstName());
+        assertEquals(expectedStudent.getLastName(), retrievedStudent.getLastName());
+        assertEquals(expectedStudent.getRegistrationNumber(), retrievedStudent.getRegistrationNumber());
+
+        String newRegistrationNumber ="309RO1232";
+        expectedStudent.setRegistrationNumber(newRegistrationNumber);
+        assertEquals(newRegistrationNumber, expectedStudent.getRegistrationNumber());
+    }
+
+    @Test
+    public void testGetStudentByYear() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date createdAt= new Date(formatter.format(date));
+        Date updatedAt= new Date(formatter.format(date));
+        Student s1 = new Student("A112", "John", "Doe", 1, "A2", "johndoe@example.com", "password", createdAt, updatedAt);
+        Student s2 = new Student("B225", "Jane", "Doe", 2, "B4", "janedoe@example.com", "mama", createdAt, updatedAt);
+        when(studentRepository.findByYear(1)).thenReturn(Arrays.asList(s1));
+        when(studentRepository.findByYear(2)).thenReturn(Arrays.asList(s2));
+
+        List<Student> students1 = studentService.getStudentByYear(1);
+        List<Student> students2 = studentService.getStudentByYear(2);
+
+        assertEquals(1, students1.size());
+        assertEquals(s1.getRegistrationNumber(), students1.get(0).getRegistrationNumber());
+        assertEquals(1, students2.size());
+        assertEquals(s2.getRegistrationNumber(), students2.get(0).getRegistrationNumber());
+    }
+
+    @Test
+    public void testGetStudentByGrupaAndYear() {
+        List<Student> expectedStudents = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date createdAt= new Date(formatter.format(date));
+        Date updatedAt= new Date(formatter.format(date));
+        expectedStudents.add(new Student("123456", "John", "Doe", 2, "A", "johndoe@example.com", "password", createdAt, updatedAt));
+        expectedStudents.add(new Student("654321", "Jane", "Doe", 2, "A", "janedoe@example.com", "mama", createdAt, updatedAt));
+        when(studentRepository.findByGrupaAndYear(anyString(), anyInt())).thenReturn(expectedStudents);
+
+        List<Student> actualStudents = studentService.getStudentByGrupaAndYear("A", 2022);
+
+        assertEquals(expectedStudents.size(), actualStudents.size());
+        assertEquals(expectedStudents.get(0).getFirstName(), actualStudents.get(0).getFirstName());
+        assertEquals(expectedStudents.get(0).getLastName(), actualStudents.get(0).getLastName());
+        assertEquals(expectedStudents.get(1).getFirstName(), actualStudents.get(1).getFirstName());
+        assertEquals(expectedStudents.get(1).getLastName(), actualStudents.get(1).getLastName());
+    }
+
+    @Test
+    void testGetStudentByFirstName() {
+        List<Student> students = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date createdAt= new Date(formatter.format(date));
+        Date updatedAt= new Date(formatter.format(date));
+        Student student1 = new Student("111111", "John", "Doe", 1, "A", "johndoe@example.com", "password", createdAt, updatedAt);
+        Student student2 = new Student("222222", "John", "Smith", 2, "B", "johnsmith@example.com", "password", createdAt, updatedAt);
+        students.add(student1);
+        students.add(student2);
+
+        when(studentRepository.findByFirstName("John")).thenReturn(students);
+
+        List<Student> result = studentService.getStudentByFirstName("John");
+        assertEquals(2, result.size());
+        assertEquals("Doe", result.get(0).getLastName());
+        assertEquals("Smith", result.get(1).getLastName());
+    }
+
+    @Test
+    void testGetStudentByLastName() {
+        List<Student> students = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date createdAt= new Date(formatter.format(date));
+        Date updatedAt= new Date(formatter.format(date));
+        Student student1 = new Student("111111", "John", "Doe", 1, "A", "johndoe@example.com", "password", createdAt, updatedAt);
+        Student student2 = new Student("222222", "Marie", "Doe", 2, "B", "mariedoe@example.com", "password", createdAt, updatedAt);
+        students.add(student1);
+        students.add(student2);
+
+        when(studentRepository.findByLastName("Doe")).thenReturn(students);
+
+        List<Student> result = studentService.getStudentByLastName("Doe");
+        assertEquals(2, result.size());
+        assertEquals("John", result.get(0).getFirstName());
+        assertEquals("Marie", result.get(1).getFirstName());
+    }
+
+    @Test
+    public void testDeleteStudent() {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Student student = new Student("111111", "John", "Doe", 1, "A", "johndoe@example.com", "password", createdAt, updatedAt);
+
+        doReturn(Optional.of(student)).when(studentRepository).findById(null);
+
+        studentService.deleteStudent(student);
+
+        verify(studentRepository, times(1)).save(any(Student.class));
+        assertEquals(true, student.getIsDeleted());
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Student student = new Student("111111", "John", "Doe", 1, "A", "johndoe@example.com", "password", createdAt, updatedAt);
+
+
+        doReturn(Optional.of(student)).when(studentRepository).findById(null);
+
+
+        String newName = "Jane";
+        String newSurname = "Smith";
+        studentService.updateStudent(student,newName, newSurname);
+
+        verify(studentRepository, times(1)).save(any(Student.class));
+
+        assertEquals(newName, student.getFirstName());
+        assertEquals(newSurname, student.getLastName());
+
+        // Alte verificari pentru set-uri:
+        UUID verificareSetId = UUID.randomUUID();
+        Integer anVerificare = 3;
+        String grupaVerificare ="B5";
+        String parolaVerificare ="bbbb";
+        String mailVerifiacre ="aaa@gmail.com";
+
+        student.setYear(anVerificare);
+        student.setId(verificareSetId);
+        student.setGrupa(grupaVerificare);
+        student.setPassword(parolaVerificare);
+        student.setMail(mailVerifiacre);
+
+        assertEquals(verificareSetId, student.getId());
+        assertEquals(grupaVerificare, student.getGrupa());
+        assertEquals(anVerificare, student.getYear());
+        assertEquals(parolaVerificare,student.getPassword());
+        assertEquals(mailVerifiacre, student.getMail());
+    }
+}
---
+++ tmp/src/test/java/com/example/Security/TeacherServiceTest.java
@@ -0,0 +1,167 @@
+package com.example.demo;
+
+import com.example.demo.objects.Teacher;
+import com.example.demo.repositories.TeacherRepository;
+import com.example.demo.services.TeacherService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TeacherServiceTest {
+    @Mock
+    private TeacherRepository teacherRepository;
+
+    @InjectMocks
+    private TeacherService teacherService;
+
+    @BeforeEach
+    public void setUp() {
+        teacherService = new TeacherService(teacherRepository);
+    }
+
+    @Test
+    public void testAddTeacher() {
+        String registrationNumber = "A112";
+        String firstName = "John";
+        String lastName = "Doe";
+        String degree = "PhD";
+        String mail = "johndoe@example.com";
+        String password = "password";
+
+        teacherService.addTeacher(firstName, lastName, degree, mail, password);
+        verify(teacherRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void testDeleteTeacher() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String degree = "PhD";
+        String mail = "johndoe@example.com";
+        String password = "password";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Teacher teacher = new Teacher(firstName, lastName, degree, mail, password, createdAt, updatedAt);
+        doReturn(Optional.of(teacher)).when(teacherRepository).findById(null);
+
+        teacherService.deleteTeacher(teacher);
+
+        verify(teacherRepository, times(1)).save(any(Teacher.class));
+        assertEquals(true, teacher.getIsDeleted());
+    }
+
+    @Test
+    public void testUpdateTeacher() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String degree = "PhD";
+        String mail = "johndoe@example.com";
+        String password = "password";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Teacher teacher = new Teacher(firstName, lastName, degree, mail, password, createdAt, updatedAt);
+
+        doReturn(Optional.of(teacher)).when(teacherRepository).findById(null);
+
+
+        String newFirstName = "Jane";
+        String newLastName = "Smith";
+        teacherService.updateTeacher(teacher,newFirstName, newLastName);
+
+        verify(teacherRepository, times(1)).save(any(Teacher.class));
+
+        assertEquals(newFirstName, teacher.getFirstName());
+        assertEquals(newLastName, teacher.getLastName());
+
+        UUID verificareSetId = UUID.randomUUID();
+        String parolaVerificare ="parola";
+        String mailVerificare = "diana@gmail.com";
+
+        teacher.setId(verificareSetId);
+        teacher.setPassword(parolaVerificare);
+        teacher.setMail(mailVerificare);
+
+        assertEquals(verificareSetId, teacher.getId());
+        assertEquals(parolaVerificare, teacher.getPassword());
+        assertEquals(mailVerificare, teacher.getMail());
+    }
+    @Test
+    void testGetTeacherByLastName() {
+        List<Teacher> teachers = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Teacher teacher1 = new Teacher("John", "Doe", "dr", "johndoe@example.com", "password", createdAt, updatedAt);
+        Teacher teacher2 = new Teacher("Marie", "Lpc", "Dr", "mariedoe@example.com", "password", createdAt, updatedAt);
+        teachers.add(teacher1);
+        teachers.add(teacher2);
+
+        when(teacherRepository.findByLastName("Doe")).thenReturn(teachers);
+
+        List<Teacher> result = teacherService.getTeacherByLastName("Doe");
+        assertEquals(2, result.size());
+        assertEquals("John", result.get(0).getFirstName());
+        assertEquals("Marie", result.get(1).getFirstName());
+    }
+    @Test
+    void testGetTeacherByDegree() {
+        List<Teacher> teachers = new ArrayList<>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        Teacher teacher1 = new Teacher("John", "Doe", "Asistent", "johndoe@example.com", "password", createdAt, updatedAt);
+        Teacher teacher2 = new Teacher("Marie", "Lpc", "Dr", "mariedoe@example.com", "password", createdAt, updatedAt);
+        teachers.add(teacher1);
+        teachers.add(teacher2);
+
+        when(teacherRepository.findByDegree("Dr")).thenReturn(teachers);
+
+        List<Teacher> result = teacherService.getTeacherByDegree("Dr");
+        assertEquals(2, result.size());
+        assertEquals("Doe", result.get(0).getLastName());
+        assertEquals("Lpc", result.get(1).getLastName());
+    }
+    @Test
+    public void testGetTeacherByMail() {
+        // Given
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        java.util.Date date = new java.util.Date();
+        java.util.Date createdAt= new java.util.Date(formatter.format(date));
+        java.util.Date updatedAt= new java.util.Date(formatter.format(date));
+        String mail = "johndoe@example.com";
+        Teacher expectedTeacher = new Teacher("John", "Doe", "Dr", "johndoe@example.com", "password", createdAt, updatedAt);
+
+        when(teacherRepository.findByMail(mail)).thenReturn(expectedTeacher);
+
+        // When
+        Teacher retrievedTeacher = teacherService.getTeacherByMail(mail);
+
+        // Then
+        Assertions.assertNotNull(retrievedTeacher);
+        assertEquals(expectedTeacher.getFirstName(), retrievedTeacher.getFirstName());
+        assertEquals(expectedTeacher.getLastName(), retrievedTeacher.getLastName());
+        assertEquals(expectedTeacher.getMail(), retrievedTeacher.getMail());
+    }
+
+}
