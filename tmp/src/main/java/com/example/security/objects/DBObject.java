022cc34ace3e82199dc417b3e70bc70935a7ceb9
db2257 <97189710+db2257@users.noreply.github.com>
2023-05-17 14:19:10 +0300

    Update DBObject.java

--- tmp/src/main/java/com/example/security/objects/DBObject.java
+++ tmp/src/main/java/com/example/security/objects/DBObject.java
@@ -39,7 +39,14 @@ public class DBObject {
     public boolean getIsDeleted() {
         return isDeleted;
     }
+    
+        public boolean isDeleted() {
+        return isDeleted;
+    }
 
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
